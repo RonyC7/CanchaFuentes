497baa00707ccbefff88cc357256de86a9e9cde8
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-05-26 10:37:44 -0600

    Guardando algo del avance por algun error

--- Clases/Program.cs
+++ Clases/Program.cs
@@ -8,9 +8,7 @@ namespace CanchaFuentes
 {
     internal static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
