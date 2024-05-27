4118414e8520176fb7798a3f3decaee35f8c80c2
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-05-26 21:41:09 -0600

    Validacion hecha

--- Formulario/FormCancha.cs
+++ Formulario/FormCancha.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace CanchaFuentes.Formulario
@@ -14,14 +15,11 @@ namespace CanchaFuentes.Formulario
         {
             InitializeComponent();
 
-            // Guardar los precios recibidos como parámetros
             this.precioDia = precioDia;
             this.precioNoche = precioNoche;
 
-            // Mostrar los precios en el label correspondiente
             lblCosto.Text = $"Precio día: Q{precioDia} / Precio noche: Q{precioNoche}";
 
-            // Cargar los horarios y tipos de horario
             CargarHorarios();
             CargarTiposHorario();
             CargarReservasRealizadas();
@@ -48,55 +46,49 @@ namespace CanchaFuentes.Formulario
 
         private void btnRReserva_Click(object sender, EventArgs e)
         {
-            // Obtener los datos ingresados por el usuario
             string nombreCliente = txtNCliente.Text;
             string telefono = txtTelefono.Text;
             string tipoHorario = comboBoxTHorario.SelectedItem.ToString();
             string horario = comboBoxHorario.SelectedItem.ToString();
             string diaReserva = dateTimePickerDiaReserva.Value.ToShortDateString();
 
-            // Validar si el horario seleccionado es válido para el tipo de horario
             if (!ValidarHorarioParaTipo(tipoHorario, horario))
             {
                 MessageBox.Show("El horario seleccionado no es válido para el tipo de horario. Por favor, selecciona un horario correcto.", "Error de Horario", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            // Validar si ya hay una reserva para el mismo horario en el mismo día
             if (ReservaExistenteEnMismoHorario(diaReserva, horario))
             {
                 MessageBox.Show("Ya existe una reserva para el mismo horario en el mismo día. Por favor, selecciona otro horario o día.", "Error de Reserva", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            // Calcular el costo de la reserva según el tipo de horario
             decimal costoReserva = CalcularCostoReserva(tipoHorario);
 
-            // Mostrar el costo de la reserva en el label
             lblCosto.Text = $"Costo de la reserva: Q{costoReserva}";
 
-            // Guardar los datos de la reserva en un archivo de texto
             GuardarReserva(nombreCliente, telefono, tipoHorario, horario, diaReserva, costoReserva);
 
-            // Mostrar mensaje de reserva realizada con éxito
             MessageBox.Show("Reserva realizada con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
 
         private bool ValidarHorarioParaTipo(string tipoHorario, string horario)
         {
             if (tipoHorario == "Dia")
             {
-                // Validar horarios para el día (de 8:00 AM a 5:00 PM)
-                if (!horario.StartsWith("8") && !horario.StartsWith("9") && !horario.StartsWith("10") && !horario.StartsWith("11") && !horario.StartsWith("12") &&
-                    !horario.StartsWith("13") && !horario.StartsWith("14") && !horario.StartsWith("15") && !horario.StartsWith("16") && !horario.StartsWith("17"))
+                if (!(horario == "8:00 AM - 9:00 AM" || horario == "9:00 AM - 10:00 AM" || horario == "10:00 AM - 11:00 AM" || horario == "11:00 AM - 12:00 PM" ||
+                      horario == "12:00 PM - 1:00 PM" || horario == "1:00 PM - 2:00 PM" || horario == "2:00 PM - 3:00 PM" || horario == "3:00 PM - 4:00 PM" ||
+                      horario == "4:00 PM - 5:00 PM"))
                 {
                     return false;
                 }
             }
             else if (tipoHorario == "Noche")
             {
-                // Validar horarios para la noche (de 6:00 PM a 11:00 PM y de 12:00 AM a 7:00 AM)
-                if (!horario.StartsWith("18") && !horario.StartsWith("19") && !horario.StartsWith("20") && !horario.StartsWith("21") && !horario.StartsWith("22") && !horario.StartsWith("23"))
+                if (!(horario == "5:00 PM - 6:00 PM" || horario == "6:00 PM - 7:00 PM" || horario == "7:00 PM - 8:00 PM" || horario == "8:00 PM - 9:00 PM" ||
+                      horario == "9:00 PM - 10:00 PM" || horario == "10:00 PM - 11:00 PM"))
                 {
                     return false;
                 }
@@ -106,23 +98,21 @@ namespace CanchaFuentes.Formulario
 
         private bool ReservaExistenteEnMismoHorario(string diaReserva, string horario)
         {
-            foreach (string reserva in reservasRealizadas)
+            return reservasRealizadas.Any(reserva =>
             {
                 string[] datosReserva = reserva.Split(',');
                 string diaReservaExistente = datosReserva[4];
                 string horarioReservaExistente = datosReserva[3];
 
-                if (diaReservaExistente == diaReserva && horarioReservaExistente == horario)
-                {
-                    return true;
-                }
-            }
-            return false;
+                return diaReservaExistente == diaReserva && horarioReservaExistente == horario;
+            });
         }
+
+
+
 
         private decimal CalcularCostoReserva(string tipoHorario)
         {
-            // Calcular el costo de la reserva según el tipo de horario seleccionado
             if (tipoHorario == "Dia")
             {
                 return precioDia;
@@ -135,7 +125,6 @@ namespace CanchaFuentes.Formulario
 
         private void GuardarReserva(string nombreCliente, string telefono, string tipoHorario, string horario, string diaReserva, decimal costoReserva)
         {
-            // Guardar los datos de la reserva en un archivo de texto (Reservas.txt)
             string datosReserva = $"{nombreCliente},{telefono},{tipoHorario},{horario},{diaReserva},{costoReserva}";
             string rutaArchivo = "Reservas.txt";
 
@@ -144,7 +133,6 @@ namespace CanchaFuentes.Formulario
                 sw.WriteLine(datosReserva);
             }
 
-            // Guardar los datos de la reserva en un archivo de texto (ReservasRealizadas.txt)
             string rutaArchivoReservasRealizadas = "ReservasRealizadas.txt";
 
             using (StreamWriter sw = File.AppendText(rutaArchivoReservasRealizadas))
@@ -155,10 +143,8 @@ namespace CanchaFuentes.Formulario
 
         private void CargarHorarios()
         {
-            // Limpiar los horarios antes de cargar nuevos
             comboBoxHorario.Items.Clear();
 
-            // Agregar horarios
             comboBoxHorario.Items.Add("8:00 AM - 9:00 AM");
             comboBoxHorario.Items.Add("9:00 AM - 10:00 AM");
             comboBoxHorario.Items.Add("10:00 AM - 11:00 AM");
@@ -175,14 +161,14 @@ namespace CanchaFuentes.Formulario
             comboBoxHorario.Items.Add("9:00 PM - 10:00 PM");
             comboBoxHorario.Items.Add("10:00 PM - 11:00 PM");
 
-            comboBoxHorario.SelectedIndex = 0; // Seleccionar el primer horario por defecto
+            comboBoxHorario.SelectedIndex = 0;
         }
 
         private void CargarTiposHorario()
         {
             comboBoxTHorario.Items.Add("Dia");
             comboBoxTHorario.Items.Add("Noche");
-            comboBoxTHorario.SelectedIndex = 0; // Seleccionar el primer tipo de horario por defecto
+            comboBoxTHorario.SelectedIndex = 0;
         }
 
         private void CargarReservasRealizadas()
@@ -200,4 +186,3 @@ namespace CanchaFuentes.Formulario
         }
     }
 }
-
