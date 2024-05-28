d39c99ab98b3589dfc097873aeec582856e1e4cb
josueg07 <josue.grc07@gmail.com>
2024-05-27 21:38:53 -0600

    a javi le gutan lojombre

--- Formulario/FormConfiguracion.Designer.cs
+++ Formulario/FormConfiguracion.Designer.cs
@@ -47,7 +47,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(716, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
@@ -62,7 +62,7 @@
             this.label1.Location = new System.Drawing.Point(117, 80);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 21);
+            this.label1.Size = new System.Drawing.Size(130, 27);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tarifa de dia";
             // 
@@ -73,7 +73,7 @@
             this.label2.Location = new System.Drawing.Point(521, 80);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 21);
+            this.label2.Size = new System.Drawing.Size(159, 27);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tarifa de noche";
             // 
@@ -85,14 +85,14 @@
             this.label3.Location = new System.Drawing.Point(241, 15);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(211, 24);
+            this.label3.Size = new System.Drawing.Size(276, 32);
             this.label3.TabIndex = 3;
             this.label3.Text = "Tarifas de la cancha";
             // 
             // txtDia
             // 
             this.txtDia.Location = new System.Drawing.Point(123, 287);
-            this.txtDia.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDia.Margin = new System.Windows.Forms.Padding(4);
             this.txtDia.Name = "txtDia";
             this.txtDia.Size = new System.Drawing.Size(132, 22);
             this.txtDia.TabIndex = 4;
@@ -100,7 +100,7 @@
             // txtNoche
             // 
             this.txtNoche.Location = new System.Drawing.Point(543, 284);
-            this.txtNoche.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNoche.Margin = new System.Windows.Forms.Padding(4);
             this.txtNoche.Name = "txtNoche";
             this.txtNoche.Size = new System.Drawing.Size(132, 22);
             this.txtNoche.TabIndex = 5;
@@ -109,7 +109,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(80, 118);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(212, 117);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -120,7 +120,7 @@
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(500, 118);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(212, 117);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -149,8 +149,9 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(333, 282);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(132, 28);
             this.button2.TabIndex = 10;
@@ -176,7 +177,7 @@
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormConfiguracion";
             this.Text = "Configuracion";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
