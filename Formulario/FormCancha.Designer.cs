5e785e320a10e9f11ea56d215d197f9de6774a6c
josueg07 <josue.grc07@gmail.com>
2024-05-27 21:36:38 -0600

    ?

--- Formulario/FormCancha.Designer.cs
+++ Formulario/FormCancha.Designer.cs
@@ -52,8 +52,9 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(637, 15);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
@@ -63,8 +64,9 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(771, 15);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 1;
@@ -76,7 +78,7 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(487, 108);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(379, 241);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -91,34 +93,36 @@
             this.label1.Location = new System.Drawing.Point(37, 15);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(293, 24);
+            this.label1.Size = new System.Drawing.Size(375, 32);
             this.label1.TabIndex = 3;
             this.label1.Text = "Reserva de cancha Fuentes";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(55, 108);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.Size = new System.Drawing.Size(114, 16);
             this.label2.TabIndex = 4;
             this.label2.Text = "Nombre Cliente";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(55, 162);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.Size = new System.Drawing.Size(69, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Telefono";
             // 
             // txtNCliente
             // 
             this.txtNCliente.Location = new System.Drawing.Point(189, 108);
-            this.txtNCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNCliente.Margin = new System.Windows.Forms.Padding(4);
             this.txtNCliente.Name = "txtNCliente";
             this.txtNCliente.Size = new System.Drawing.Size(265, 22);
             this.txtNCliente.TabIndex = 6;
@@ -126,15 +130,16 @@
             // txtTelefono
             // 
             this.txtTelefono.Location = new System.Drawing.Point(189, 153);
-            this.txtTelefono.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(4);
             this.txtTelefono.Name = "txtTelefono";
             this.txtTelefono.Size = new System.Drawing.Size(265, 22);
             this.txtTelefono.TabIndex = 7;
             // 
             // btnRReserva
             // 
+            this.btnRReserva.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRReserva.Location = new System.Drawing.Point(711, 368);
-            this.btnRReserva.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRReserva.Margin = new System.Windows.Forms.Padding(4);
             this.btnRReserva.Name = "btnRReserva";
             this.btnRReserva.Size = new System.Drawing.Size(155, 28);
             this.btnRReserva.TabIndex = 8;
@@ -146,7 +151,7 @@
             // 
             this.comboBoxTHorario.FormattingEnabled = true;
             this.comboBoxTHorario.Location = new System.Drawing.Point(189, 197);
-            this.comboBoxTHorario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxTHorario.Margin = new System.Windows.Forms.Padding(4);
             this.comboBoxTHorario.Name = "comboBoxTHorario";
             this.comboBoxTHorario.Size = new System.Drawing.Size(265, 24);
             this.comboBoxTHorario.TabIndex = 9;
@@ -154,7 +159,7 @@
             // dateTimePickerDiaReserva
             // 
             this.dateTimePickerDiaReserva.Location = new System.Drawing.Point(189, 279);
-            this.dateTimePickerDiaReserva.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePickerDiaReserva.Margin = new System.Windows.Forms.Padding(4);
             this.dateTimePickerDiaReserva.Name = "dateTimePickerDiaReserva";
             this.dateTimePickerDiaReserva.Size = new System.Drawing.Size(265, 22);
             this.dateTimePickerDiaReserva.TabIndex = 10;
@@ -162,20 +167,22 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(55, 251);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(118, 16);
+            this.label4.Size = new System.Drawing.Size(135, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "Horario de cancha";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(55, 287);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 16);
+            this.label5.Size = new System.Drawing.Size(110, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Dia de reserva";
             // 
@@ -192,17 +199,19 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(60, 334);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(108, 16);
+            this.label7.Size = new System.Drawing.Size(125, 16);
             this.label7.TabIndex = 14;
             this.label7.Text = "Costo de alquiler";
             // 
             // button4
             // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(471, 15);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(135, 28);
             this.button4.TabIndex = 15;
@@ -213,10 +222,11 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(55, 207);
             this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(99, 16);
+            this.label8.Size = new System.Drawing.Size(114, 16);
             this.label8.TabIndex = 16;
             this.label8.Text = "Tipo de horario";
             // 
@@ -224,7 +234,7 @@
             // 
             this.comboBoxHorario.FormattingEnabled = true;
             this.comboBoxHorario.Location = new System.Drawing.Point(189, 241);
-            this.comboBoxHorario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxHorario.Margin = new System.Windows.Forms.Padding(4);
             this.comboBoxHorario.Name = "comboBoxHorario";
             this.comboBoxHorario.Size = new System.Drawing.Size(265, 24);
             this.comboBoxHorario.TabIndex = 17;
@@ -254,7 +264,7 @@
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormCancha";
             this.Text = "Cancha";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
